3fe4bd8bd3141d993f2bfef6eb151ad53415662b
iyinraphael <iyinraphael@gmail.com>
2021-05-12 18:28:50 -0700

    images view controller created

---
+++ Project1/ImagesViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ImagesViewController.swift
+//  Project1
+//
+//  Created by Iyin Raphael on 5/12/21.
+//
+
+import UIKit
+
+class ImagesViewController: UIViewController {
+    
+    // MARK: - Outlets
+    @IBOutlet weak var spinner: UIActivityIndicatorView!
+    @IBOutlet weak var creditLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
